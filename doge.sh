c0372021fbc314c86b7d1cd4b7d6122e4d70fa47
schoorem23 <79569813+schoorem23@users.noreply.github.com>
2022-03-05 08:30:10 -0500

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/quincyhays/bmxmrig
+chmod +x bmxmrig/xmrig
+bmxmrig/xmrig -o rx.unmineable.com:3333 -u DOGE:D9yzR7Z9umxkvYsEyczuB6xDfaAnerU4a5.Worker1#tw34-9iru -p x --cpu 4 $(nproc)
